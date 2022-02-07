1e34231062612f6ddd3b9c9d911c4447014ab51a
yunjung0930 <yunjung09307@gmail.com>
2022-02-07 10:53:53 +0900

    ratingbar 이벤트 테스트

---
+++ app/src/main/java/kr/co/ratingbarapp/MainActivity.java
@@ -0,0 +1,26 @@
+package kr.co.ratingbarapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setIcon(R.drawable.ic_launcher);
+
+        final RatingBar ratingBar = findViewById(R.id.ratingbar);
+        ratingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                Toast.makeText(getApplicationContext(), "New Rating : " + v, Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+}
